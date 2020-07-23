e61337259441a1da6aabb1096fa3118fb286d8c4
maxoumax <44801188+maxoumax@users.noreply.github.com>
2020-07-23 15:16:53 +1100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,26 @@
+let y2 = 0
+let y1 = 0
+let y = 0
+let x = 0
+basic.forever(function () {
+    y2 = -2
+    y1 = -1
+    y = 0
+    x = randint(0, 4)
+    basic.pause(500)
+    while (y2 != 5) {
+        led.plot(x, y2)
+        led.plot(x, y1)
+        led.plot(x, y)
+        led.plotBrightness(x, y, 255)
+        led.plotBrightness(x, y1, 185)
+        led.plotBrightness(x, y2, 115)
+        basic.pause(500)
+        led.unplot(x, y2)
+        led.unplot(x, y1)
+        led.unplot(x, y)
+        y2 += 1
+        y1 += 1
+        y += 1
+    }
+})
